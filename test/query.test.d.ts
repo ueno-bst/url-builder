13edb68c24d52d71b3564b054d6b167b48c783ed
t-ueno <ueno@biost.jp>
2023-04-24 15:54:49 +0900

    いろいろリフレッシュ

--- test/query.test.d.ts
+++ test/query.test.d.ts
@@ -1,2 +1,1 @@
 export {};
-//# sourceMappingURL=query.test.d.ts.map
--- test/query.test.js
+++ test/query.test.js
@@ -3,32 +3,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mocha_1 = require("mocha");
 const chai_1 = require("chai");
 const index_1 = require("../index");
-mocha_1.describe("QueryBuilder Test", () => {
-    mocha_1.it("Full URL", () => {
+(0, mocha_1.describe)("QueryBuilder Test", () => {
+    (0, mocha_1.it)("Full URL", () => {
         const url = "http://test.com/path?query_args=example#hash";
         const object = new index_1.URLBuilder(url);
         chai_1.assert.equal(object.build(), url);
     });
-    mocha_1.it("Domain Only", () => {
+    (0, mocha_1.it)("Domain Only", () => {
         const i = new index_1.URLBuilder("test.com");
         chai_1.assert.equal(i.build(), "test.com");
     });
-    mocha_1.it("Domain + Port", () => {
+    (0, mocha_1.it)("Domain + Port", () => {
         const url = "test.com:80";
         const object = new index_1.URLBuilder(url);
         chai_1.assert.equal(object.build(), url);
     });
-    mocha_1.it("Schema + User + Password + Domain + Port", () => {
+    (0, mocha_1.it)("Schema + User + Password + Domain + Port", () => {
         const url = "https://user:password@test.com:4080/";
         const object = new index_1.URLBuilder(url);
         chai_1.assert.equal(object.build(), url);
     });
-    mocha_1.it("User + Password + Domain + Port", () => {
+    (0, mocha_1.it)("User + Password + Domain + Port", () => {
         const url = "user:password@test.com:4080";
         const object = new index_1.URLBuilder(url);
         chai_1.assert.equal(object.build(), url);
     });
-    mocha_1.it("Set Query 1", () => {
+    (0, mocha_1.it)("Set Query 1", () => {
         const url = "test.com:4000/index";
         const object = new index_1.URLBuilder(url);
         object.query.set("bool", "");
